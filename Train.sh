b1cfe755711ad1d34d805f53208850523e397c4c
tuanmanh1410 <tuanmanh.bka@gmail.com>
2023-01-31 14:14:28 +0900

    Add explaination for train process

--- Train.sh
+++ Train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Make shell script for training Custom Dataset
 
-COCO="../COLOR_5K_COCO"
-NUM_CLASSES=7
+COCO="../COLOR_5K_COCO" # Path to Dataset following COCO format
+NUM_CLASSES=7 # Number of classes in your dataset
 NUM_EPOCHS=100
 BATCH_SIZE=2
 NUM_GPU=4
@@ -22,11 +22,27 @@ else
   cd ./detr_Egg_Detection
 fi
 
+# Firstly, we need to download the pretrained model with no class head for preparing training own dataset
 python Get_checkpoint.py
 
+# Train the model
+# --nproc_per_node: Number of GPUs
+# --use_env: Use environment variables to pass arguments
+# --coco_path: Path to Dataset following COCO format
+# --resume: Path to pretrained model
+# --batch_size: Batch size
+# --num_classes: Number of classes in your dataset
+# --output_dir: Path to save the model
+# --epochs: Number of epochs
 python -m torch.distributed.launch --nproc_per_node=$NUM_GPU --use_env main.py --coco_path $COCO --resume ./detr-r50_no-class-head.pth \
 --batch_size $BATCH_SIZE --num_classes $NUM_CLASSES --output_dir $OUTPUT --epochs $NUM_EPOCHS
 
+# Evaluate the model after training
+# --coco_path: Path to Dataset following COCO format
+# --batch_size: Batch size
+# --num_classes: Number of classes in your dataset
+# --resume: Path to pretrained model
+# --detail: Show the detail of evaluation of bbox
 python final_evaluate.py --coco_path $COCO --batch_size $BATCH_SIZE --num_classes $NUM_CLASSES --resume $OUTPUT/best.pth --detail True
 
 # Move to main folder
