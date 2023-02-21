187cd137c7b86e409c148a9f81ff5c686427d916
Tuan Manh Tao <tuanmanh.bka@gmail.com>
2023-02-21 20:14:38 +0900

    Update Inference script

--- Inference.sh
+++ Inference.sh
@@ -5,11 +5,10 @@ COCO_dir = $1
 COCO="../$COCO_dir"
 NUM_CLASSES=$2
 BATCH_SIZE=$3
-OUTPUT=$4
-CHECK_POINT=$5
+CHECK_POINT=$4
 
 cd ./detr_Egg_Detection
-python final_evaluate.py --coco_path $COCO --batch_size $BATCH_SIZE --num_classes $NUM_CLASSES --resume $OUTPUT/$CHECK_POINT --detail True
+python final_evaluate.py --coco_path $COCO --batch_size $BATCH_SIZE --num_classes $NUM_CLASSES --resume $CHECK_POINT --detail True
 
 # Move to main folder
 cd ..
